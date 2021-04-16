014e7c6a746f42a2d7eeed00dd99e73c34f26c57
summerscar <shen.yus@163.com>
2021-04-16 16:04:40 +0800

    🐞fix(wheel event): 使用原生事件代替合成时间

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ const TVList = TVListConfig as TVtype[]
 const randomTV: () => TVtype = () => TVList[Math.floor(Math.random() * TVList.length)]
 
 function App() {
+  const selectorRef = useRef<HTMLDivElement>()
   const dplayer = useRef<DPlayer>()
   const [currentTV, setCurrentTV] = useState<TVtype>()
   const originTitle = useRef(window.document.title)
@@ -23,7 +24,7 @@ function App() {
 
   const containerDeg = useMemo(() => {
     if (!currentTV) return 0
-    
+
     if (currentTV !== prevTV.current) {
       if (!prevTV.current) {
         prevTV.current = TVList[0]
@@ -32,11 +33,11 @@ function App() {
       let delt = TVList.indexOf(currentTV) - TVList.indexOf(prevTV.current)
 
       let deltDeg = delt * -1 * 360 / TVList.length
-      deltDeg = Math.abs(deltDeg) > 180 
-        ? deltDeg > 0 
-          ? -360 + deltDeg : 
-            360 + deltDeg 
-          : deltDeg
+      deltDeg = Math.abs(deltDeg) > 180
+        ? deltDeg > 0
+          ? -360 + deltDeg :
+          360 + deltDeg
+        : deltDeg
       let deg = prevDeg.current + deltDeg
       prevDeg.current = deg
       prevTV.current = currentTV
@@ -64,6 +65,28 @@ function App() {
     }
     setTVInfo(tv)
   }, [])
+
+  useEffect(() => {
+    const func = (e: WheelEvent) => {
+      e.preventDefault()
+      const prev = currentTV
+      const prevIndex = TVList.indexOf(prev!)
+      let nextIndex
+      if (e.deltaY > 0) {
+        nextIndex = prevIndex + 1
+      } else {
+        nextIndex = prevIndex - 1
+      }
+      nextIndex = nextIndex > TVList.length - 1
+        ? 0
+        : nextIndex < 0
+          ? TVList.length - 1
+          : nextIndex
+      setTVInfo(TVList[nextIndex])
+    }
+    selectorRef.current!.addEventListener('wheel', func)
+    return () => selectorRef.current!.removeEventListener('wheel', func)
+  }, [currentTV])
 
   const createDplayer = useCallback(() => {
     if (!currentTV) return
@@ -94,40 +117,25 @@ function App() {
   return (
     <div className="App" id="App">
       <h2>Nihon TV</h2>
-      <div className="selector" onWheel={e => {
-        const prev = currentTV
-        const prevIndex = TVList.indexOf(prev!)
-        let nextIndex
-        if (e.deltaY > 0) {
-          nextIndex = prevIndex + 1
-        } else {
-          nextIndex = prevIndex - 1
-        }
-        nextIndex = nextIndex > TVList.length - 1
-          ? 0 
-          : nextIndex < 0 
-            ? TVList.length - 1
-            : nextIndex
-        setTVInfo(TVList[nextIndex])
-      }}>
-        <div className="container" style={{transform: `translateX(-50%) rotateY(${containerDeg}deg)`}}>
-        {
+      <div className="selector" ref={selectorRef}>
+        <div className="container" style={{ transform: `translateX(-50%) rotateY(${containerDeg}deg)` }}>
+          {
             TVList.map((tv, index, arr) => (
-              <div 
-                className="tvImg" 
-                key={tv.title} 
-                style={{transform: `rotateY(${index * 360 / arr.length }deg ) translateZ(300px)`}}
+              <div
+                className="tvImg"
+                key={tv.title}
+                style={{ transform: `rotateY(${index * 360 / arr.length}deg ) translateZ(300px)` }}
                 onClick={() => {
                   setTVInfo(tv)
                 }}
               >
-                <img src={`img/${tv.img}`}/>
+                <img src={`img/${tv.img}`} />
               </div>
             ))
           }
-        </div>      
+        </div>
       </div>
-      <div style={{padding: '0 0 20px'}}>
+      <div style={{ padding: '0 0 20px' }}>
         <span>TV：</span>
         <select
           value={currentTV?.src}
@@ -136,38 +144,39 @@ function App() {
             setTVInfo(tv!)
           }}
         >
-          {TVList.map(tv => <option key={tv.src} value={tv.src}>{ tv.title }</option>)}
+          {TVList.map(tv => <option key={tv.src} value={tv.src}>{tv.title}</option>)}
         </select>
         <button onClick={() => window.open('https://tv.yahoo.co.jp/listings')} style={{ marginLeft: '0.8rem' }}>番組表</button>
       </div>
       <div id="dplayer" />
       <table>
-        <tr>
-          <td>月曜から夜更かし</td>
-          <td>日テレ</td>
-          <td>月曜日　24時</td>
-        </tr>
-        <tr>
-          <td>恋はdeepに</td>
-          <td>日テレ</td>
-          <td>水曜日　22時</td>
-        </tr>
-        <tr>
-          <td>桜の塔</td>
-          <td>テレビ朝日</td>
-          <td>木曜日　21時</td>
-        </tr>
-        <tr>
-          <td>龍桜</td>
-          <td>ＴＢＳ</td>
-          <td>木曜日　ー</td>
-        </tr>
-        <tr>
-          <td>ネメシス</td>
-          <td>日テレ</td>
-          <td>日曜日　22時30分</td>
-        </tr>
-
+        <tbody>
+          <tr>
+            <td>月曜から夜更かし</td>
+            <td>日テレ</td>
+            <td>月曜日　24時</td>
+          </tr>
+          <tr>
+            <td>恋はdeepに</td>
+            <td>日テレ</td>
+            <td>水曜日　22時</td>
+          </tr>
+          <tr>
+            <td>桜の塔</td>
+            <td>テレビ朝日</td>
+            <td>木曜日　21時</td>
+          </tr>
+          <tr>
+            <td>龍桜</td>
+            <td>ＴＢＳ</td>
+            <td>木曜日　ー</td>
+          </tr>
+          <tr>
+            <td>ネメシス</td>
+            <td>日テレ</td>
+            <td>日曜日　22時30分</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
